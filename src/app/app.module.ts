2ba586a913b7168117c1dd61b3300fd485121956
Vinicius Silva Oliveira <vinicius.oliveira@ibm.com>
2021-07-02 11:39:33 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { MainComponent } from './core/main/main.component';
+import { BannerComponent } from './core/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  slideIndex = 0;
+
+  constructor(private renderer: Renderer2,) { }
+
+  ngOnInit(): void {
+    this.showSlides();
+  }
+
+  showSlides() {
+    let i;
+    let slides = document.getElementsByClassName('mySlides');
+
+
+    for (i = 0; i < slides.length; i++) {
+      this.renderer.setStyle(slides[i], 'display', 'none')
+      this.renderer.removeClass(slides[i], 'animation');
+    }
+    this.slideIndex++;
+    if (this.slideIndex > slides.length) { this.slideIndex = 1 }
+
+    this.renderer.setStyle(slides[this.slideIndex - 1], 'display', 'block');
+    this.renderer.addClass(slides[this.slideIndex - 1], 'animation');
+    setTimeout(() => {
+      this.showSlides();
+    }, 2000);
+  }
+
+}
