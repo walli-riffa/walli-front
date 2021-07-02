3ec1d171c2aad97c2a295c121777027097303bc1
nfandre <andrelive.05@hotmail.com>
2021-07-02 15:54:48 -0300

    fix: variaveis de ambinte

--- environment.js
+++ environment.js
@@ -2,9 +2,7 @@ const fs = require('fs');
 
 const heroku = `export const environment = {
     production: true,
-    NZESPORTES_API: '${process.env.NZESPORTES_API}',
-    TOKEN_HASH_KEY: '${process.env.TOKEN_HASH_KEY}',
-    USER_HASH_KEY: '${process.env.USER_HASH_KEY}'
+    WALLI_API: '${process.env.WALLI_API}'
 };`
 
 fs.writeFile('src/environments/environment.prod.ts', heroku, (err, result) => {
