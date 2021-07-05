2925a3f04db6f14022147330a4bee0fd9fac2770
nfandre <andrelive.05@hotmail.com>
2021-07-05 13:42:35 -0300

    fix: token rash

--- environment.js
+++ environment.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 
 const heroku = `export const environment = {
     production: true,
-    WALLI_API: '${process.env.WALLI_API}'
+    WALLI_API: '${process.env.WALLI_API}',
+    TOKEN_HASH_KEY: '${process.env.TOKEN_HASH_KEY}',
+    USER_HASH_KEY: '${process.env.USER_HASH_KEY}'
 };`
 
 fs.writeFile('src/environments/environment.prod.ts', heroku, (err, result) => {
