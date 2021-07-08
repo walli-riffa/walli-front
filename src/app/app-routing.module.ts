548042820a93ed14a86a4d140acdf64c0ea5aa68
Vinicius Silva Oliveira <vinicius.oliveira@ibm.com>
2021-07-08 18:06:01 -0300

    tela de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './core/main/main.component';
+import { LoginComponent } from './dashbord/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent
-  }
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
 ];
 
 @NgModule({
