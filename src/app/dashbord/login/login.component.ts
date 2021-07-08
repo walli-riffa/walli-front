19cc09dcd81aba8d9b6e8ace5ef9e9cc9569c807
nfandre <andrelive.05@hotmail.com>
2021-07-08 19:02:26 -0300

    fix lint

--- src/app/dashbord/login/login.component.ts
+++ src/app/dashbord/login/login.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
-import { map } from 'rxjs/operators';
+import {Router, ActivatedRoute} from '@angular/router';
+import {map} from 'rxjs/operators';
 
 
 @Component({
@@ -11,19 +11,20 @@ import { map } from 'rxjs/operators';
 })
 export class LoginComponent implements OnInit {
   public loginData: FormGroup = new FormGroup({});
-  public loginForm!: any;
 
   constructor(
     private formBuilder: FormBuilder
   ) {
   }
+
   ngOnInit(): void {
     this.createForm();
   }
+
   private createForm(): void {
     this.loginData = this.formBuilder.group({
       user: new FormControl('', Validators.required),
-      password: new FormControl( '', Validators.required),
+      password: new FormControl('', Validators.required),
     });
   }
 
